e3e558c525aeb291225846c140f1e1692f1eb03f
PythonZem <114096673+PythonZem@users.noreply.github.com>
2024-04-01 11:44:51 +0300

    add [book]: Add Book model

--- book/models.py
+++ book/models.py
@@ -1,3 +1,16 @@
+from enum import Enum
+
+from django.core.validators import MinValueValidator
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=60)
+    author = models.CharField(max_length=60, blank=True)
+    cover = models.CharField(
+        max_length=60, choices=Enum('Cover', ['HARD', 'SOFT'])
+    )
+    inventory = models.IntegerField(
+        validators=[MinValueValidator(1)],
+    )
+    daily_fee = models.DecimalField(max_digits=10, decimal_places=2)
