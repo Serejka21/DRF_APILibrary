d35eed619853e4be0108eb55ec975263c568e31a
Maksym Operchuk <operlast529@gmail.com>
2024-04-01 13:52:44 +0300

    add: null=True, blank=True parameters to actual_return_date field.

--- borrowings/models.py
+++ borrowings/models.py
@@ -4,6 +4,6 @@ from django.db import models
 class Borrowing(models.Model):
     borrow_date = models.DateTimeField()
     expected_return_date = models.DateTimeField()
-    actual_return_date = models.DateTimeField()
+    actual_return_date = models.DateTimeField(null=True, blank=True)
     book = models.OneToOneField("Book")  # TODO: remove mock
     user = models.ForeignKey("User")     # TODO: remove mock
