81b56f1246d573a6587f80ac2759224a4ab77ec3
PythonZem <114096673+PythonZem@users.noreply.github.com>
2024-04-01 17:45:13 +0300

    add [book]: Add test

---
+++ book/tests/test_book_api.py
@@ -0,0 +1,107 @@
+from decimal import Decimal
+
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+
+from rest_framework.test import APIClient
+from rest_framework import status
+
+from book.models import Book
+from book.serializers import BookSerializer
+
+BOOK_URL = reverse("book:book-list")
+
+
+def sample_book(**params):
+    defaults = {
+        "title": "Sample book",
+        "author": "Same author",
+        "cover": "HARD",
+        "inventory": 1,
+        "daily_fee": "4.64"
+    }
+    defaults.update(params)
+
+    return Book.objects.create(**defaults)
+
+
+def detail_url(book_id):
+    return reverse("book:book-detail", args=[book_id])
+
+
+class UnauthenticatedBookApiTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_auth_required(self):
+        res = self.client.get(BOOK_URL)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+
+class AuthenticatedMovieApiTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = get_user_model().objects.create_user(
+            "test@test.com",
+            "testpass",
+        )
+        self.client.force_authenticate(self.user)
+
+    def test_list_books(self):
+        sample_book()
+        sample_book()
+
+        res = self.client.get(BOOK_URL)
+
+        books = Book.objects.order_by("id")
+        serializer = BookSerializer(books, many=True)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
+
+    def test_retrieve_book_detail(self):
+        book = sample_book()
+
+        url = detail_url(book.id)
+        res = self.client.get(url)
+
+        serializer = BookSerializer(book)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
+
+    def test_create_book_forbidden(self):
+        payload = {
+            "title": "Sample book",
+            "author": "Same author",
+            "cover": "HARD",
+            "inventory": -1,
+            "daily_fee": Decimal("4.64")
+        }
+        res = self.client.post(BOOK_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_403_FORBIDDEN)
+
+
+class AdminMovieApiTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = get_user_model().objects.create_user(
+            "admin@admin.com", "testpass", is_staff=True
+        )
+        self.client.force_authenticate(self.user)
+
+    def test_create_book(self):
+        payload = payload = {
+            "title": "Sample book",
+            "author": "Same author",
+            "cover": "HARD",
+            "inventory": 1,
+            "daily_fee": Decimal("4.64")
+        }
+        res = self.client.post(BOOK_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        book = Book.objects.get(id=res.data["id"])
+        for key in payload.keys():
+            self.assertEqual(payload[key], getattr(book, key))
