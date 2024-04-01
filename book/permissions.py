c9378aefa4de2fc181fe6c201c3067a34872f588
Serhii Buryk <87121473+Serejka21@users.noreply.github.com>
2024-04-01 14:58:55 +0300

    Update: add docstring to perm class

--- book/permissions.py
+++ book/permissions.py
@@ -2,6 +2,7 @@ from rest_framework.permissions import SAFE_METHODS, BasePermission
 
 
 class IsAdminOrReadOnly(BasePermission):
+    """Permission to CRUD with admin-role or readonly for simple users"""
     def has_permission(self, request, view):
         return bool(
             (request.method in SAFE_METHODS and request.user)
