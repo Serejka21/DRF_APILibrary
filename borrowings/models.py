a21deeee75a216879635188cb5990af07983a8c0
Maksym Operchuk <operlast529@gmail.com>
2024-04-01 13:01:56 +0300

    add: TODO comments to borrowing model.

--- borrowings/models.py
+++ borrowings/models.py
@@ -5,5 +5,5 @@ class Borrowing(models.Model):
     borrow_date = models.DateTimeField()
     expected_return_date = models.DateTimeField()
     actual_return_date = models.DateTimeField()
-    book = models.OneToOneField("Book")
-    user = models.ForeignKey("User")
+    book = models.OneToOneField("Book")  # TODO: remove mock
+    user = models.ForeignKey("User")     # TODO: remove mock
