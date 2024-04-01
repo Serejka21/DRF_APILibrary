e6f766a54b1bfc3c910961d51a8e9e37898686d1
Maksym Operchuk <operlast529@gmail.com>
2024-04-01 19:53:18 +0300

    upgrade: dockstring in BorrowingViewSet.

--- borrowings/views.py
+++ borrowings/views.py
@@ -6,7 +6,6 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 
 from borrowings.models import Borrowing
-from borrowings.serializers import BorrowingSerializer, BorrowingDetailSerializer, BorrowingCreateSerializer
 from borrowings.serializers import (
     BorrowingSerializer,
     BorrowingDetailSerializer,
