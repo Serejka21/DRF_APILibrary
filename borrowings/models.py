7dbfe7ef40eb2711ead71ea5ed9523da0272ec3b
Maksym Operchuk <operlast529@gmail.com>
2024-04-01 15:12:18 +0300

    add: dockstring for Borrowing model.

--- borrowings/models.py
+++ borrowings/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 
 class Borrowing(models.Model):
+    """Borrowing model."""
+
     borrow_date = models.DateTimeField()
     expected_return_date = models.DateTimeField()
     actual_return_date = models.DateTimeField(null=True, blank=True)
