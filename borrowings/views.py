82de9047231600a997271fa7bf8113274e454d3a
Maksym Operchuk <operlast529@gmail.com>
2024-04-01 13:13:38 +0300

    add: serializer_class to BorrowingViewSet.

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,8 +1,9 @@
 from rest_framework import viewsets
 
 from borrowings.models import Borrowing
+from borrowings.serializers import BorrowingSerializer
 
 
 class BorrowingViewSet(viewsets.ModelViewSet):
     queryset = Borrowing.objects.all()
-
+    serializer_class = BorrowingSerializer()
