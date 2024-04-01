9c7af41791439fc553484a4f71b4c5236b961ea2
Mykola <mykola22dubyna@gmail.com>
2024-04-01 13:20:49 +0300

    Fix(user): add slash to path

--- DRF_API_Library/urls.py
+++ DRF_API_Library/urls.py
@@ -21,5 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("__debug__/", include("debug_toolbar.urls")),
-    path("api/user", include("user.urls", namespace="user")),
+    path("api/user/", include("user.urls", namespace="user")),
 ]
