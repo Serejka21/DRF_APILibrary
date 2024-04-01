5c56519349f61b539439b6bccf6b1a36e33f3aca
Maksym Operchuk <operlast529@gmail.com>
2024-04-01 13:02:57 +0300

    create: BorrowingViewSet with only queryset field.

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from borrowings.models import Borrowing
+
+
+class BorrowingViewSet(viewsets.ModelViewSet):
+    queryset = Borrowing.objects.all()
+
