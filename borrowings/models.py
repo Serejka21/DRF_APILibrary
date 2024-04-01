8142522d365454bfc7c8aef87319db37e9f2fe9b
Maksym Operchuk <operlast529@gmail.com>
2024-04-01 12:52:57 +0300

    add: borrowing model.

--- borrowings/models.py
+++ borrowings/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Borrowing(models.Model):
+    borrow_date = models.DateTimeField()
+    expected_return_date = models.DateTimeField()
+    actual_return_date = models.DateTimeField()
+    book = models.OneToOneField("Book")
+    user = models.ForeignKey("User")
