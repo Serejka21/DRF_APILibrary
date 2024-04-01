526b643240164e19d8e9d9912badbe1b9d688b1c
Maksym Operchuk <operlast529@gmail.com>
2024-04-01 15:47:22 +0300

    upgrade: borrow_date field by adding auto_now_add parameter in Borrowing model.

--- borrowings/models.py
+++ borrowings/models.py
@@ -7,7 +7,7 @@ from book.models import Book
 class Borrowing(models.Model):
     """Borrowing model."""
 
-    borrow_date = models.DateTimeField()
+    borrow_date = models.DateTimeField(auto_now_add=True)
     expected_return_date = models.DateTimeField()
     actual_return_date = models.DateTimeField(null=True, blank=True)
     book = models.OneToOneField(Book, on_delete=models.CASCADE)
