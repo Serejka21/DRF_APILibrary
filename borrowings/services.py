4b770db40a4020fb102fadec2f7b965640db8f36
Maksym Operchuk <operlast529@gmail.com>
2024-04-02 14:35:10 +0300

    implement: borrowing_create_validation function.

--- borrowings/services.py
+++ borrowings/services.py
@@ -1,9 +1,16 @@
-from django.http import HttpRequest
+from collections import OrderedDict
+from datetime import datetime, timedelta
+
+import pytz
+from django.conf import settings
 from django.db.models.query import QuerySet
+from rest_framework import status
+from rest_framework.exceptions import ValidationError
+from rest_framework.request import Request
 
 
 def filtering(
-        queryset: QuerySet, request: HttpRequest
+        queryset: QuerySet, request: Request
 ) -> QuerySet:
     user_id = request.query_params.get("user_id")
     is_active = request.query_params.get("is_active")
@@ -22,3 +29,27 @@ def filtering(
             queryset = queryset.filter(actual_return_date__isnull=False)
 
     return queryset
+
+
+def borrowing_create_validation(attrs: OrderedDict) -> OrderedDict:
+    expected_return_date = attrs["expected_return_date"]
+
+    project_tz = pytz.timezone(f"{settings.TIME_ZONE}")
+    now = datetime.now(tz=project_tz)
+    now_plus_one_day = now + timedelta(days=1)
+
+    if expected_return_date < now_plus_one_day:
+        raise ValidationError(
+            "Borrow time can`t be lass than one day",
+            code=status.HTTP_403_FORBIDDEN
+        )
+
+    book_inventory = attrs["book"].inventory
+
+    if book_inventory <= 0:
+        raise ValidationError(
+            "Sorry but all such books were taken away",
+            code=status.HTTP_403_FORBIDDEN
+        )
+
+    return attrs
