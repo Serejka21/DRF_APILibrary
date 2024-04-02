0ad936c84ec3041a0e1e5c6871217b49033473d7
Maksym Operchuk <operlast529@gmail.com>
2024-04-02 17:06:30 +0300

    implement: sample_borrowing function.

--- borrowings/tests/test_borrowings_api.py
+++ borrowings/tests/test_borrowings_api.py
@@ -4,8 +4,31 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APIClient
 
+from book.models import Book
+from borrowings.models import Borrowing
+from borrowings.serializers import BorrowingSerializer
+from user.models import User
 
 BORROWING_URL = reverse("borrowings:borrowing-list")
+
+
+def sample_borrowing(**params):
+    book = Book.objects.create(
+        title="testBook",
+        author="testAuthor",
+        inventory=5,
+        daily_fee=2
+    )
+
+    defaults = {
+        "expected_return_date": "2024-06-07",
+        "book": book,
+        "user": params["user"]
+    }
+
+    defaults.update(params)
+
+    Borrowing.objects.create(**defaults)
 
 
 class UnauthenticatedBorrowingApiTests(TestCase):
