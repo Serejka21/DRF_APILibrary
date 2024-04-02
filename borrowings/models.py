a970ddf781de4f41ec37d9e11d4d569a84acafb9
PythonZem <114096673+PythonZem@users.noreply.github.com>
2024-04-02 12:59:57 +0300

    add [book]: add Protection of the book from destroy if it has min 1 borrowing

--- borrowings/models.py
+++ borrowings/models.py
@@ -10,5 +10,13 @@ class Borrowing(models.Model):
     borrow_date = models.DateTimeField(auto_now_add=True)
     expected_return_date = models.DateTimeField()
     actual_return_date = models.DateTimeField(null=True, blank=True)
-    book = models.ForeignKey(Book, on_delete=models.PROTECT)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
+    book = models.ForeignKey(
+        Book,
+        on_delete=models.PROTECT,
+        related_name="borrowings"
+    )
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.PROTECT,
+        related_name="borrowings"
+    )
