7f08d5bca6f7948cb6773dfeea4e542f5128cfd9
Mykola <mykola22dubyna@gmail.com>
2024-04-03 12:47:44 +0300

    Add(scheduler): tasks to handle overdue

--- borrowings/tasks.py
+++ borrowings/tasks.py
@@ -1,8 +1,11 @@
 import requests
 
+from datetime import timedelta
+
 from celery import shared_task
 
 from DRF_API_Library import settings
+from borrowings.models import Borrowing
 
 
 TOKEN = settings.TOKEN
@@ -25,3 +28,37 @@ def send_borrowing_created_notification(
                f"Borrow date: {borrow_date}.\n"
                f"Expected return date: {expected_return_date}")
     requests.post(f"{URL}{message}")
+
+
+@shared_task()
+def send_borrowing_overdue_notification(
+    borrow_id: str,
+    user_email: str,
+    borrow_date,
+    expected_return_date,
+) -> None:
+    """Use telegram API to send notification about borrowing overdue"""
+    message = (f"!!!Borrowing {borrow_id}"
+               f" is overdue by {user_email}!!!\n"
+               f"Borrow date: {borrow_date}.\n"
+               f"Expected return date: {expected_return_date}")
+    requests.post(f"{URL}{message}")
+
+
+@shared_task()
+def check_borrowings_for_overdue() -> None:
+    """Check borrowings for overdue and send notification about borrowings status"""
+    overdue_missing = True
+    for borrowing in Borrowing.objects.all():
+        if borrowing.borrow_date - borrowing.expected_return_date <= timedelta(days=1):
+            send_borrowing_overdue_notification.delay(
+                borrowing.id,
+                borrowing.user.email,
+                borrowing.borrow_date,
+                borrowing.expected_return_date
+            )
+            overdue_missing = False
+
+    if overdue_missing:
+        message = "No borrowings overdue today!"
+        requests.post(f"{URL}{message}")
