f43b19633ebdc4e9298793857a0a2876a1c52693
Mykola <mykola22dubyna@gmail.com>
2024-04-03 12:47:11 +0300

    Update(scheduler): settings.py
    - add SECRET_KEY to env
    - add django_celery_beat to installed apps
    - change db to postgresql
    - change time zone to Europe/Kiev

--- DRF_API_Library/settings.py
+++ DRF_API_Library/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-o_+@h%byg86*a(87!3bc$e6#p*+zx-$1ztaiwro_u+8j_jo^ct"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "debug_toolbar",
     "drf_spectacular",
+    "django_celery_beat",
     "borrowings",
     "user",
     "book",
@@ -88,14 +89,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "DRF_API_Library.wsgi.application"
 
-
-# Database
-# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": os.getenv("POSTGRES_HOST"),
+        "PORT": os.getenv("POSTGRES_PORT"),
     }
 }
 
@@ -124,7 +125,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Europe/Kiev"
 
 USE_I18N = True
 
