987fc7a9e1150793f5b53bf16dafc3f0fa8ba9f5
PythonZem <114096673+PythonZem@users.noreply.github.com>
2024-04-03 15:38:46 +0300

    add [documentation]: Add swagger-ui for documentation API

--- DRF_API_Library/urls.py
+++ DRF_API_Library/urls.py
@@ -17,6 +17,11 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+    SpectacularRedocView,
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -25,4 +30,15 @@ urlpatterns = [
     path("api/library/", include("user.urls", namespace="user")),
     path("api/library/", include("book.urls", namespace="book")),
     path("api/library/", include("payment.urls", namespace="payment")),
+    path("api/library/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/library/schema/swagger-ui/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    path(
+        "api/library/schema/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
+    ),
 ]
