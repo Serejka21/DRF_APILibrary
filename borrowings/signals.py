5e781fa7b05bef822f85965556c18b9b0b47a121
Mykola <mykola22dubyna@gmail.com>
2024-04-03 09:38:30 +0300

    Add(notifications): signals for borrowing and payment apps

---
+++ borrowings/signals.py
@@ -0,0 +1,21 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from borrowings.tasks import send_borrowing_created_notification
+from borrowings.models import Borrowing
+
+
+@receiver(post_save, sender=Borrowing, dispatch_uid="post_save_signal_processed")
+def send_borrowing_created_message(sender, instance, created, **kwargs):
+    """Handle creation of Borrowing"""
+    try:
+        if created:
+            send_borrowing_created_notification.delay(
+                instance.id,
+                instance.user.email,
+                instance.borrow_date,
+                instance.expected_return_date,
+            )
+    except Exception as e:
+        print("Error sending created borrowing notification!")
+        print(e)
---
+++ payment/signals.py
@@ -0,0 +1,22 @@
+from django.dispatch import receiver, Signal
+
+from payment.tasks import send_success_payment_notification
+from payment.models import Payment
+
+
+successful_payment = Signal()
+
+
+@receiver(successful_payment, sender=Payment, dispatch_uid="post_save_signal_processed")
+def send_success_payment_message(sender, instance, created, **kwargs):
+    """Handle successful payment"""
+    if created:
+        try:
+            send_success_payment_notification.delay(
+                instance.borrowing.id,
+                instance.borrowing.user.email,
+                instance.money_to_pay
+            )
+        except Exception as e:
+            print("Error sending created borrowing notification!")
+            print(e)
